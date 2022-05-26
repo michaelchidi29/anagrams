eb1e7edf78089f4a1d88f8a46a1197aeeb15dc21
michaelchidi29 <105291680+michaelchidi29@users.noreply.github.com>
2022-05-26 18:08:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,17 @@
 
 def find_anagram(word, anagram):
     # [assignment] Add your code here
+    
+    str1 = input ("1word:")
+str2 = input ("2word")
+sorted_str1 = sorted(str1)
+sorted_str2 = sorted(str2)
+ 
+if sorted_str1 == sorted_str2:
+     print("True")
+else:
+         print("False")
 
-    return True
+    
+      
 
